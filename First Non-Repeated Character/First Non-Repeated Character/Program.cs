f6b6d724caf023b54dfa8105c80456510d4c8a00
JacobSkyrme <skyrmejacob@gmail.com>
2017-10-11 21:07:51 +0100

    First Non-Repeated Character Algorithem

--- First Non-Repeated Character/First Non-Repeated Character/Program.cs
+++ First Non-Repeated Character/First Non-Repeated Character/Program.cs
@@ -10,6 +10,40 @@ namespace First_Non_Repeated_Character
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please input any string.");
+            char[] allChars = Console.ReadLine().ToCharArray();
+
+            Dictionary<char, int> charCounter = new Dictionary<char, int>();
+
+            for(int i = 0; i < allChars.Length;i++)
+            {
+                char currentChar = allChars[i];
+                if(charCounter.ContainsKey(currentChar))
+                {
+                    charCounter[currentChar]++;
+                }
+                else
+                {
+                    charCounter.Add(currentChar, 1);
+                }
+            }
+            bool noRepeatChar = false;
+            for (int i = 0; i < allChars.Length; i++)
+            {
+                char currentChar = allChars[i];
+
+                if (charCounter[currentChar] == 1)
+                {
+                    Console.WriteLine("The first repeating character is " + currentChar);
+                    noRepeatChar = true;
+                    break;
+                }
+            }
+            if (noRepeatChar == false)
+            {
+                Console.WriteLine("There were no repeating characters.");
+            }
+            Console.ReadLine();
         }
     }
 }
