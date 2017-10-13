af770e7a2b7c75857c348765e1e744c9ef1ee69f
Jacob Skyrme <skyrmejacob@gmail.com>
2017-10-14 00:27:09 +0100

    String reverse program

---
+++ String Reversal/String Reversal/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Reversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a string to be reversed");
+            string input = Console.ReadLine();
+            char[] allCharacters = input.ToCharArray();
+            string IterativeOutput = "";
+            string recursiveOutput = new string(input.ToCharArray().Reverse().ToArray());
+
+            for (int i = 0;  i < allCharacters.Length; i++)
+            {
+                IterativeOutput += allCharacters[allCharacters.Length - i -1];
+            }
+            Console.WriteLine("Iterative Reversed " + IterativeOutput);
+            Console.WriteLine("Recursively Reversed " + recursiveOutput); 
+            Console.ReadLine();
+
+        }
+    }
+}
